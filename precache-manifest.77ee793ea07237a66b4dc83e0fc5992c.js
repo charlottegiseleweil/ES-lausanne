d92a42d27a57c7a7243f26f98a4a295167151d9d
Charlotte Gisele Weil <weilcharlotte@gmail.com>
2021-02-23 17:13:49 +0100

    Updates

---
+++ precache-manifest.77ee793ea07237a66b4dc83e0fc5992c.js
@@ -0,0 +1,182 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d637c0d5fb12c9588b8060f9837e036",
+    "url": "/ES-lausanne/index.html"
+  },
+  {
+    "revision": "c34dd2b0ba768c0d7207",
+    "url": "/ES-lausanne/static/css/2.0b5659ac.chunk.css"
+  },
+  {
+    "revision": "028399a937ca2bfbb069",
+    "url": "/ES-lausanne/static/css/main.d75b6272.chunk.css"
+  },
+  {
+    "revision": "c34dd2b0ba768c0d7207",
+    "url": "/ES-lausanne/static/js/2.33f9749c.chunk.js"
+  },
+  {
+    "revision": "11f86ddc566c30b6cad223d8a078311f",
+    "url": "/ES-lausanne/static/js/2.33f9749c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "028399a937ca2bfbb069",
+    "url": "/ES-lausanne/static/js/main.98ce2612.chunk.js"
+  },
+  {
+    "revision": "3bfcd077d73611d628eb",
+    "url": "/ES-lausanne/static/js/runtime-main.6de68bba.js"
+  },
+  {
+    "revision": "259d6ecf9142b73c4953cb2f36c24efb",
+    "url": "/ES-lausanne/static/media/ABBALegend.259d6ecf.png"
+  },
+  {
+    "revision": "fc5969d4bc0f2dd4230b537c64ae53e0",
+    "url": "/ES-lausanne/static/media/ABBA_Dataset.fc5969d4.zip"
+  },
+  {
+    "revision": "c13fa0899ff1182ab56946d9f8d2463f",
+    "url": "/ES-lausanne/static/media/AOI.c13fa089.zip"
+  },
+  {
+    "revision": "6664085334b4fc5b97b19c05abbb48d7",
+    "url": "/ES-lausanne/static/media/Beijing.66640853.jpg"
+  },
+  {
+    "revision": "92a84ec95ef9d7a20ef1cbb42e7baa5c",
+    "url": "/ES-lausanne/static/media/Olympics.92a84ec9.jpg"
+  },
+  {
+    "revision": "f8041b0394cc02fa9256be370287c198",
+    "url": "/ES-lausanne/static/media/Watersheds.f8041b03.zip"
+  },
+  {
+    "revision": "a831c7165bb54c334bbed2ff05b76a89",
+    "url": "/ES-lausanne/static/media/af.a831c716.png"
+  },
+  {
+    "revision": "20f19f9e5a7f8298bb041346bf62e25d",
+    "url": "/ES-lausanne/static/media/airplane.20f19f9e.svg"
+  },
+  {
+    "revision": "eefba0acd878390c34d3a6ad0ac1e134",
+    "url": "/ES-lausanne/static/media/bayBridge.eefba0ac.jpg"
+  },
+  {
+    "revision": "9164b5e1513a5129bf768725701fa913",
+    "url": "/ES-lausanne/static/media/bldg-cover.9164b5e1.tif"
+  },
+  {
+    "revision": "db2bf4cd8f729b58f914c26c7ae70489",
+    "url": "/ES-lausanne/static/media/city.db2bf4cd.svg"
+  },
+  {
+    "revision": "32406ab8abb5edf5820cf907b0d1a799",
+    "url": "/ES-lausanne/static/media/cw.32406ab8.png"
+  },
+  {
+    "revision": "a92836372bfc0bfbe07a48a5627e01f1",
+    "url": "/ES-lausanne/static/media/doubleLeaf.a9283637.svg"
+  },
+  {
+    "revision": "f9068c05277360aad1a5245e86e3aa2a",
+    "url": "/ES-lausanne/static/media/exampleLegend4.f9068c05.png"
+  },
+  {
+    "revision": "8ce20f1f4ff4c5658bd9c1fa5075dcbe",
+    "url": "/ES-lausanne/static/media/forest.8ce20f1f.svg"
+  },
+  {
+    "revision": "137612a416f30b34f59ad10dc2d81f0b",
+    "url": "/ES-lausanne/static/media/halloween.137612a4.jpg"
+  },
+  {
+    "revision": "b1b2edf8be2c33bf750558378bda5ed8",
+    "url": "/ES-lausanne/static/media/headerImg.b1b2edf8.png"
+  },
+  {
+    "revision": "c1483f09127724564073e36c8ff34bbe",
+    "url": "/ES-lausanne/static/media/heat-mitigation-0.25-4326.c1483f09.tif"
+  },
+  {
+    "revision": "49a8224353278da91b1f8f8f0ad99758",
+    "url": "/ES-lausanne/static/media/heat-mitigation-0.5-4326.49a82243.tif"
+  },
+  {
+    "revision": "e3f8d9fb5dc7f3c44971310a48bc6983",
+    "url": "/ES-lausanne/static/media/heat-mitigation-0.75-4326.e3f8d9fb.tif"
+  },
+  {
+    "revision": "11ed5d7ef55f07358dbe599111ff1196",
+    "url": "/ES-lausanne/static/media/heat-mitigation-1.0-4326.11ed5d7e.tif"
+  },
+  {
+    "revision": "18bb21a79bc8d8119a8063a01c34bb7a",
+    "url": "/ES-lausanne/static/media/herrin.18bb21a7.jpg"
+  },
+  {
+    "revision": "6fac5a756d01617d54eec174a96366dc",
+    "url": "/ES-lausanne/static/media/house.6fac5a75.svg"
+  },
+  {
+    "revision": "630971fb41a0fe2f816a295bf9eff547",
+    "url": "/ES-lausanne/static/media/leaf.630971fb.svg"
+  },
+  {
+    "revision": "087bbae76bbc32a657f792dd3c66498a",
+    "url": "/ES-lausanne/static/media/minneapolis.087bbae7.jpg"
+  },
+  {
+    "revision": "1053ed6f79c0f1fb66402d40cb45e8c7",
+    "url": "/ES-lausanne/static/media/natCapAdventures.1053ed6f.zip"
+  },
+  {
+    "revision": "c6a9c80e61161204827462d8e1213897",
+    "url": "/ES-lausanne/static/media/natcap.c6a9c80e.png"
+  },
+  {
+    "revision": "8ac0faef57d3116b718290f220060876",
+    "url": "/ES-lausanne/static/media/nationalParks.8ac0faef.zip"
+  },
+  {
+    "revision": "c2ce20e8789eac9eb06bff205a02bb51",
+    "url": "/ES-lausanne/static/media/no-profile-img.c2ce20e8.png"
+  },
+  {
+    "revision": "80b85c0259678788d670c1b61c38b5eb",
+    "url": "/ES-lausanne/static/media/norrkoping.80b85c02.png"
+  },
+  {
+    "revision": "2e23d06d99569744b010926cc623c9b9",
+    "url": "/ES-lausanne/static/media/puertoMaldonado.2e23d06d.jpg"
+  },
+  {
+    "revision": "d2249e85847a392570312e6aae1c5f47",
+    "url": "/ES-lausanne/static/media/redmond.d2249e85.jpg"
+  },
+  {
+    "revision": "a2778aa91b27776c6e1d041197904b46",
+    "url": "/ES-lausanne/static/media/stockholm.a2778aa9.jpg"
+  },
+  {
+    "revision": "433105b39cfa03337ab1aef7b8383f60",
+    "url": "/ES-lausanne/static/media/tot_c_cur_LULC09_18-4326.433105b3.tif"
+  },
+  {
+    "revision": "ce75f10885b02361766f985683cab9ce",
+    "url": "/ES-lausanne/static/media/tot_c_cur_LULC18_18-4326.ce75f108.tif"
+  },
+  {
+    "revision": "fa2914da1e187487151c62a3e49ec0e8",
+    "url": "/ES-lausanne/static/media/tot_c_cur_LULC85_18-4326.fa2914da.tif"
+  },
+  {
+    "revision": "ce910bdf0884e60bffc064f4088656b0",
+    "url": "/ES-lausanne/static/media/tot_c_cur_LULC97_18-4326.ce910bdf.tif"
+  },
+  {
+    "revision": "e734bea51caca1f902c41a483ff793e5",
+    "url": "/ES-lausanne/static/media/wyoming.e734bea5.jpg"
+  }
+]);
